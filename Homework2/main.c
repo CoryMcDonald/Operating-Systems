c001fd8f7e9db868b0789b85b0a9583ae8f4be65
Cory McDonald <cory@corywmcdonald.com>
2014-09-27 17:20:25 -0500

    Added unit test and made 23/27 return code examples work

--- Homework2/main.c
+++ Homework2/main.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <stdbool.h>
 
 #define BUFSIZE 1024
 #define CSTRSIZE 100
@@ -15,13 +16,9 @@
 
 #define DEBUG 1
 
-#define SIZE_OF_FIRST_COMMAND 16
-#define SIZE_OF_SECOND_COMMAND 16
-
-char *firstCommand[SIZE_OF_FIRST_COMMAND];
 
 int parse_command(char *line, char **cmd1, char **cmd2, char *infile, char *outfile);
-
+void returnTest();
 int main ( int argc, char *argv[] )
 {
     char infile[CSTRSIZE];
@@ -69,14 +66,282 @@ int main ( int argc, char *argv[] )
     }
     else
     {
-        printf("usage error\n");
+        returnTest();
+        // bool active = true;
+        // char userCommand[BUFSIZE];
+        // while (active)
+        // {
+        //     printf("myshell-%% ");
+        //     //This makes sure we don't go past the limit of 256 characters. That would be very, very bad
+        //     fgets (userCommand, 256, stdin);
+        //     //Removing newline from command
+        //     if ((strlen(userCommand) > 0) && (userCommand[strlen (userCommand) - 1] == '\n'))
+        //         userCommand[strlen (userCommand) - 1] = '\0';
+
+        //     if (userCommand != NULL)
+        //     {
+
+        //         i = parse_command(userCommand, cmd1, cmd2, infile, outfile);
+        //         printf("return code is %d\n", i);
+
+        //         if ( i < 9)
+        //         {
+        //             k = 0;
+        //             while (cmd1[k] != NULL)
+        //             {
+        //                 // printf("cmd1[%d] = %s\n", k, cmd1[k]);
+        //                 k++;
+        //             };
+        //             k = 0;
+        //             while (cmd2[k] != NULL)
+        //             {
+        //                 // printf("cmd2[%d] = %s\n", k, cmd2[k]);
+        //                 k++;
+        //             };
+        //             if (strlen(infile))
+        //             {
+        //                 // printf("input redirection file name: %s\n", infile);
+        //             }
+        //             if (strlen(outfile))
+        //             {
+        //                 // printf("output redirection file name: %s\n", outfile);
+        //             }
+        //         }
+        //     }
+        // }
+        // printf("usage error\n");
     }
     return 0;
 }
+void returnTest()
+{
+    char infile[CSTRSIZE];
+    char outfile[CSTRSIZE];
+    char *cmd1[CMDSIZE];
+    char *cmd2[CMDSIZE];
+    int i;
+    int success =0;
 
+    cmd1[0] = NULL;
+    cmd2[0] = NULL;
+    char returnCode1Test1[50] = "ls";
+    char returnCode1Test2[50] = "ls –l";
+    char returnCode1Test3[50] = "ls –l –a";
+
+    char returnCode2Test1[50] = "wc < filename";
+
+    char returnCode3Test1[50] = "ls >> outputfile";
+    char returnCode3Test2[50] = "ls –l >> outputfile";
+    char returnCode3Test3[50] = "ls –l –a >> outputfile";
+    char returnCode3Test4[50] = "wc < filename >> outputfile";
+
+    char returnCode4Test1[50] = "ls > outputfile";
+    char returnCode4Test2[50] = "ls –l > outputfile";
+    char returnCode4Test3[50] = "ls –l –a > outputfile";
+    char returnCode4Test4[50] = "wc < filename > outputfile";
+
+
+    char returnCode5Test1[50] = "ls | grep c";
+    char returnCode5Test2[50] = "ls –l | grep c";
+    char returnCode5Test3[50] = "ls –l –a | grep c";
+
+    char returnCode6Test1[50] = "wc < filename | grep 3";
+
+    char returnCode7Test1[50] = "ls | grep c >> outputfile";
+    char returnCode7Test2[50] = "ls –l | grep c >> outputfile";
+    char returnCode7Test3[50] = "ls –l –a | grep c >> outputfile";
+    char returnCode7Test4[50] = "wc < filename | grep 3 >> outputfile";
+
+    char returnCode8Test1[50] = "ls | grep c > outputfile";
+    char returnCode8Test2[50] = "ls –l | grep c > outputfile";
+    char returnCode8Test3[50] = "ls –l –a | grep c > outputfile";
+    char returnCode8Test4[50] = "wc < filename | grep 3 > outputfile";
+
+    char returnCode9Test1[50] = "cat file1 | grep c | wc";
+    char returnCode9Test2[50] = "netbeans&";
+
+
+    i = parse_command(returnCode1Test1, cmd1, cmd2, infile, outfile);
+    if(i==1) { success++; } else { printf("%s test failed. 1 != %d\n", returnCode1Test1 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+    i = parse_command(returnCode1Test2, cmd1, cmd2, infile, outfile);
+    if(i==1) { success++; } else { printf("%s test failed. 1 != %d\n", returnCode1Test2 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+    i = parse_command(returnCode1Test3, cmd1, cmd2, infile, outfile);
+    if(i==1) { success++; } else { printf("%s test failed. 1 != %d\n", returnCode1Test2 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+
+    i = parse_command(returnCode2Test1, cmd1, cmd2, infile, outfile);
+    if(i==2) { success++; } else { printf("%s test failed. 2 != %d\n", returnCode2Test1 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+
+    i = parse_command(returnCode3Test1, cmd1, cmd2, infile, outfile);
+    if(i==3) { success++; } else { printf("%s test failed. 3 != %d\n", returnCode3Test1 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+    i = parse_command(returnCode3Test2, cmd1, cmd2, infile, outfile);
+    if(i==3) { success++; } else { printf("%s test failed. 3 != %d\n", returnCode3Test2 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+    i = parse_command(returnCode3Test3, cmd1, cmd2, infile, outfile);
+    if(i==3) { success++; } else { printf("%s test failed. 3 != %d\n", returnCode3Test3 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+    i = parse_command(returnCode3Test4, cmd1, cmd2, infile, outfile);
+    if(i==3) { success++; } else { printf("%s test failed. 3 != %d\n", returnCode3Test4 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+
+    i = parse_command(returnCode4Test1, cmd1, cmd2, infile, outfile);
+    if(i==4) { success++; } else { printf("%s test failed. 1 != %d\n", returnCode4Test1 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+    i = parse_command(returnCode4Test2, cmd1, cmd2, infile, outfile);
+    if(i==4) { success++; } else { printf("%s test failed. 1 != %d\n", returnCode4Test2 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+    i = parse_command(returnCode4Test3, cmd1, cmd2, infile, outfile);
+    if(i==4) { success++; } else { printf("%s test failed. 1 != %d\n", returnCode4Test3 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+    i = parse_command(returnCode4Test4, cmd1, cmd2, infile, outfile);
+    if(i==4) { success++; } else { printf("%s test failed. 4 != %d\n", returnCode4Test4 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+
+    i = parse_command(returnCode5Test1, cmd1, cmd2, infile, outfile);
+    if(i==5) { success++; } else { printf("%s test failed. 5 != %d\n", returnCode5Test1 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+    i = parse_command(returnCode5Test2, cmd1, cmd2, infile, outfile);
+    if(i==5) { success++; } else { printf("%s test failed. 5 != %d\n", returnCode5Test2 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+    i = parse_command(returnCode5Test3, cmd1, cmd2, infile, outfile);
+    if(i==5) { success++; } else { printf("%s test failed. 5 != %d\n", returnCode5Test3 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+
+    i = parse_command(returnCode6Test1, cmd1, cmd2, infile, outfile);
+    if(i==6) { success++; } else { printf("%s test failed. 6 != %d\n", returnCode6Test1 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+
+    i = parse_command(returnCode7Test1, cmd1, cmd2, infile, outfile);
+    if(i==7) { success++; } else { printf("%s test failed. 7 != %d\n", returnCode7Test1 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+    i = parse_command(returnCode7Test2, cmd1, cmd2, infile, outfile);
+    if(i==7) { success++; } else { printf("%s test failed. 7 != %d\n", returnCode7Test2 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+    i = parse_command(returnCode7Test3, cmd1, cmd2, infile, outfile);
+    if(i==7) { success++; } else { printf("%s test failed. 7 != %d\n", returnCode7Test3 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+    i = parse_command(returnCode7Test4, cmd1, cmd2, infile, outfile);
+    if(i==7) { success++; } else { printf("%s test failed. 7 != %d\n", returnCode7Test4 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+
+
+    i = parse_command(returnCode8Test1, cmd1, cmd2, infile, outfile);
+    if(i==8) { success++; } else { printf("%s test failed. 8 != %d\n", returnCode8Test1 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+    i = parse_command(returnCode8Test2, cmd1, cmd2, infile, outfile);
+    if(i==8) { success++; } else { printf("%s test failed. 8 != %d\n", returnCode8Test2 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+    i = parse_command(returnCode8Test3, cmd1, cmd2, infile, outfile);
+    if(i==8) { success++; } else { printf("%s test failed. 8 != %d\n", returnCode8Test3 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+    i = parse_command(returnCode8Test4, cmd1, cmd2, infile, outfile);
+    if(i==8) { success++; } else { printf("%s test failed. 8 != %d\n", returnCode8Test4 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+
+    i = parse_command(returnCode9Test1, cmd1, cmd2, infile, outfile);
+    if(i==9) { success++; } else { printf("%s test failed. 9 != %d\n", returnCode9Test1 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+    i = parse_command(returnCode9Test2, cmd1, cmd2, infile, outfile);
+    if(i==9) { success++; } else { printf("%s test failed. 9 != %d\n", returnCode9Test2 ,i); } i=0;cmd1[0] = NULL; cmd2[0] = NULL;
+
+    printf("%d/26 tests completed successfully", success);
+}
 
 int parse_command(char *line, char **cmd1, char **cmd2, char *infile, char *outfile)
 {
-    int returnCode = 0;
+    int returnCode = 9;
+    int cmd1Index = 0;
+    int cmd2Index = 0;
+
+    char delimin[2] = " ";
+    char outputRedirectedTo[3]; //Could be >>,>,<
+    char *token;
+    char *copyOfLine = (char *) malloc (strlen(line) + 1);
+
+    bool isOutputRedirected = false;
+    bool pipe = false;
+    bool reset = true;
+    copyOfLine = strcpy(copyOfLine, line);
+    token = strtok(copyOfLine, delimin); //Tokenizing
+
+    while (token != NULL)
+    {
+        if (strstr(token, "quit") || strstr(token, "exit")) //Quiting
+        {
+            printf("Program terminates successfully by the user\n");
+            returnCode = 0;
+            break;
+        }
+        else if (reset == true) //Taking in command, otherwise we will assume it is an argument
+        {
+            reset = false;
+            //Return code stuff
+            if (pipe == true)
+            {
+
+                cmd2[cmd2Index] = token + '\0';
+                cmd2Index++;
+
+                returnCode = 5;
+            }
+            else
+            {
+                //Simple command
+                cmd1[cmd1Index] = token + '\0';
+                cmd1Index++;
+
+                returnCode = 1;
+            }
+        }
+        else if (strstr(token, "|")) //Piping include a space
+        {
+            reset = true;
+            pipe = true;
+            if (strlen(token) > 1) //more than 1 character, an argument has been attached to it
+            {
+                char substringToken[3];
+                memcpy( substringToken, &token[0], 2 ); //Figure out what the hell i'm doing here.
+                substringToken[2] = '\0';
+                // printf("The command line argument to the user command and program is: [%s]\n", substringToken );
+            }
+            // printf("Pipe: yes\n");
+        }
+        else if (strstr(token, ">>") || strstr(token, ">") || strstr(token, "<")) //Output redirected
+        {
+            isOutputRedirected = true;
+            strncpy(outputRedirectedTo, token, sizeof(outputRedirectedTo));
+            outputRedirectedTo[sizeof(outputRedirectedTo) - 1] = '\0';
+            // printf("Output Direction: %s\n", token);
+        }
+        else if (isOutputRedirected == true)
+        {
+            if (strstr(outputRedirectedTo, ">>"))
+            {
+                if (pipe == true)
+                {
+                    returnCode = 7;
+                }
+                else
+                {
+                    returnCode = 3;
+                }
+            }
+            else if (strstr(outputRedirectedTo, ">"))
+            {
+                if (pipe == true)
+                {
+                    returnCode = 8;
+                }
+                else
+                {
+                    returnCode = 4;
+                }
+            }
+            else if (strstr(outputRedirectedTo, "<"))
+            {
+                if (pipe == true)
+                {
+                    returnCode = 6;
+                }
+            }
+        }
+        else
+        {
+            if (pipe == true)
+            {
+                cmd2[cmd2Index] =  token + '\0';
+                cmd2Index++;
+            }
+            else
+            {
+                cmd1[cmd1Index] =  token + '\0';
+                cmd1Index++;
+            }
+        }
+
+
+        token = strtok(NULL, delimin);
+    }
+    cmd1[cmd1Index] = NULL;
+    cmd2[cmd2Index] = NULL;
     return returnCode;
 }
